6312277274e7eddbab1b9e092999a8cdc52611e0
Nguyen Duc Dung <gnudevx@gmail.com>
2026-06-25 13:45:16 +0700

    "socket"

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:5000', {
+export const socket = io(process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000', {
   withCredentials: true,
   autoConnect: false,
 });
